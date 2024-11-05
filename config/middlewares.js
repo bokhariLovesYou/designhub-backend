c2aa8b986e5f3a3d5a6f3774ae6bca8410951a52
Paras Bokhari <parasbokhari@gmail.com>
2024-11-05 15:08:50 -0600

    updating middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,37 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true, // deprecated in v4.25.8
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "https://designhub-api.up.railway.app",
+        "https://designhubfe.netlify.app",
+      ],
+    },
+  },
+  // 'strapi::cors',
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
